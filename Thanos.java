d0f88d519ba202855d1690a40cdb314d1a80891d
ganout <morgan.bourdillel@gmail.com>
2020-03-30 10:55:23 +0200

    with a snap of the fingers

--- Thanos.java
+++ Thanos.java
@@ -4,24 +4,55 @@ public class Thanos {
 
     public static void main(String[] args) {
 
-        // TODO 1 : Create an empty heroes list
 
-        // TODO 2 : Add those heroes to the list
-        // name: Black Widow, age: 34
-        // name: Captain America, age: 100
-        // name: Vision, age: 3
-        // name: Iron Man, age: 48
-        // name: Scarlet Witch, age: 29
-        // name: Thor, age: 1500
-        // name: Hulk, age: 49
-        // name: Doctor Strange, age: 42
+        ArrayList <Hero> heroes = new ArrayList<>(); 
 
-        // TODO 3 : It's Thor birthday, now he's 1501
+        Hero captainAmerica = new Hero("Captain America", 100);
+        Hero blackWidow = new Hero("Black Widow", 34);
+        Hero vision = new Hero("Vision", 3);
+        Hero ironMan = new Hero("Iron Man", 48);
+        Hero scarletWitch = new Hero("Scarlet Witch", 29);
+        Hero thor = new Hero("Thor", 1500);
+        Hero hulk = new Hero("Hulk", 49);
+        Hero doctorStrange = new Hero("Doctor Strange", 42);
 
-        // TODO 4 : Shuffle the heroes list
+        heroes.add(captainAmerica);
+        heroes.add(blackWidow);
+        heroes.add(vision);
+        heroes.add(ironMan);
+        heroes.add(scarletWitch);
+        heroes.add(thor);
+        heroes.add(hulk);
+        heroes.add(doctorStrange);
 
-        // TODO 5 : Keep only the half of the list
+        thor.setAge(1501);
 
-        // TODO 6 : Loop throught the list and display the name of the remaining heroes
+        Collections.shuffle(heroes);
+
+        System.out.println("\n");
+        System.out.println("The heroes fighting Thanos are :");
+        System.out.println("--------------------------------" + "\n");
+
+        int i = 1;
+        for (Hero hero : heroes) {
+            System.out.println(" " + i + ". " + hero.getName());
+            i++;
+        }
+
+        System.out.println("\n");
+        System.out.println("Thanos snaps his fingers...");
+        System.out.println("\n");
+
+        List <Hero> halfHeroes = heroes.subList(0, (heroes.size() / 2));
+
+        System.out.println("The surviving heroes are :");
+        System.out.println("--------------------------" + "\n");
+
+        int j = 1;
+        for (Hero hasSurvived : halfHeroes) {
+            System.out.println(" " + j + ". " + hasSurvived.getName());
+            j++;
+        }
+        System.out.println("\n");
     }
 }
